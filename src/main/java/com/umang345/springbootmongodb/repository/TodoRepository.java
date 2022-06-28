31bc9ae5f3fb005dbdad80108df285686961b8b4
Umang <umangagarwal345@gmail.com>
2022-06-28 17:54:47 +0530

    Added Get Todo Item by todo in MongoRepository

--- src/main/java/com/umang345/springbootmongodb/repository/TodoRepository.java
+++ src/main/java/com/umang345/springbootmongodb/repository/TodoRepository.java
@@ -2,8 +2,14 @@ package com.umang345.springbootmongodb.repository;
 
 import com.umang345.springbootmongodb.model.TodoDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface TodoRepository extends MongoRepository<TodoDTO, String> {
+public interface TodoRepository extends MongoRepository<TodoDTO, String>
+{
+    @Query("{'todo': ?0}")
+    Optional<TodoDTO> findByTodo(String todo);
 }
