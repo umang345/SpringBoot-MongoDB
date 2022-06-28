611010e822c07b541df5bd50800afdf9274044fa
Umang <umangagarwal345@gmail.com>
2022-06-28 17:47:34 +0530

    Added not null configuration

--- src/main/java/com/umang345/springbootmongodb/model/TodoDTO.java
+++ src/main/java/com/umang345/springbootmongodb/model/TodoDTO.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Getter
@@ -19,10 +20,13 @@ public class TodoDTO
     @Id
     private String id;
 
+    @NotNull(message = "todo cannot be null")
     private String todo;
 
+    @NotNull(message = "description cannot be null")
     private String description;
 
+    @NotNull(message = "completed cannot be null")
     private Boolean completed;
 
     private Date createdAt;
